de8da3f795694707f58f4f0a389e05930b84ecf4
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-08-17 18:59:35 +0600

    feat: add India country configuration interface and constants

---
+++ src/countries/india/config.ts
@@ -0,0 +1,13 @@
+export interface IndiaCountryConfig {
+  countryCode: string
+  countryName: string
+  supportedDomains: string[]
+  officialPortalUrl: string
+}
+
+export const INDIA_COUNTRY_CONFIG: IndiaCountryConfig = {
+  countryCode: 'IND',
+  countryName: 'India',
+  supportedDomains: ['indianvisaonline.gov.in'],
+  officialPortalUrl: 'https://indianvisaonline.gov.in/',
+}
