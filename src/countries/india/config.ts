3fe5a0a0c3ba259c3d06e49235df10c8ede952d0
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-09-12 23:54:06 +0600

    feat: add India visa autofill selectors, UI pages, and test suite

--- src/countries/india/config.ts
+++ src/countries/india/config.ts
@@ -11,3 +11,14 @@ export const INDIA_COUNTRY_CONFIG: IndiaCountryConfig = {
   supportedDomains: ['indianvisaonline.gov.in', 'indianvisa-bangladesh.nic.in'],
   officialPortalUrl: 'https://indianvisaonline.gov.in/',
 }
+
+export interface IndiaBangladeshWorkflowDefaults {
+  defaultMission: string
+  temporaryEmailDomain: string
+}
+
+export const BANGLADESH_WORKFLOW_DEFAULTS: IndiaBangladeshWorkflowDefaults = {
+  defaultMission: 'BANGLADESH-RAJSHAHI',
+  temporaryEmailDomain: 'temporary-visa-app.com',
+}
+
