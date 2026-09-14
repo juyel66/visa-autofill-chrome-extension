1155dc2ed1f5638716665d510d04c0f6455b4464
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-09-15 00:01:09 +0600

    test: configure test environment with JSDOM and DOMMatrix mocks

--- tests/setup.ts
+++ tests/setup.ts
@@ -23,4 +23,21 @@ global.CSS = {
 // Mock DOMMatrix for pdfjs-dist in Node environment
 global.DOMMatrix = class MockDOMMatrix {
   a = 1; b = 0; c = 0; d = 1; e = 0; f = 0;
+  m11 = 1; m12 = 0; m13 = 0; m14 = 0;
+  m21 = 0; m22 = 1; m23 = 0; m24 = 0;
+  m31 = 0; m32 = 0; m33 = 1; m34 = 0;
+  m41 = 0; m42 = 0; m43 = 0; m44 = 1;
+  is2D = true;
+  isIdentity = true;
+  constructor(init?: any) {
+    if (Array.isArray(init) && init.length === 6) {
+      this.a = init[0]; this.b = init[1]; this.c = init[2]; this.d = init[3]; this.e = init[4]; this.f = init[5];
+    }
+  }
+  multiply() { return this; }
+  translate() { return this; }
+  scale() { return this; }
+  rotate() { return this; }
+  inverse() { return this; }
+  transformPoint(p?: any) { return p || { x: 0, y: 0, z: 0, w: 1 }; }
 } as any
